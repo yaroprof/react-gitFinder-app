0377cce301a6d9ccaff2eb7a467b1bf23aac3fe3
Yaroslav <crosskyiv@gmail.com>
2022-05-18 18:28:57 +0300

    adding main pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 
@@ -8,7 +11,15 @@ function App() {
       <Router>
         <div className="flex flex-col justify-between h-screen">
           <Navbar/>
-          <main className='container mx-auto px-3 pb-12'>Content</main>
+          <main className='container mx-auto px-3 pb-12'>
+            <Routes>
+              <Route path='/' element={<Home/>}></Route>
+              <Route path='/About' element={<About/>}></Route>
+              <Route path='/NotFound' element={<NotFound/>}></Route>
+              <Route path='/*' element={<NotFound/>}></Route>
+            </Routes>
+
+          </main>
           <Footer/>
         </div>
       </Router>
