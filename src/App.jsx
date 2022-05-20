60c5e0708fac4d99cfc2327d8046ea84d412b0c8
Yaroslav <crosskyiv@gmail.com>
2022-05-20 11:55:35 +0300

    setup github Context

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import About from './pages/About';
 import NotFound from './pages/NotFound';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
+import {GithubProvider} from './context/github/GithubContext'
+
 
 
 function App() {
   return (
+    <GithubProvider>
       <Router>
         <div className="flex flex-col justify-between h-screen">
           <Navbar/>
@@ -23,6 +26,8 @@ function App() {
           <Footer/>
         </div>
       </Router>
+    </GithubProvider>
+
         
 
   );
