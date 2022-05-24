8d08411904d33a3e762a3009f1b962e330650772
Yaroslav <crosskyiv@gmail.com>
2022-05-24 12:38:31 +0300

    save

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,8 +5,7 @@ function About() {
 	<>
       <h1 className='text-6xl mb-4'>Github Finder</h1>
       <p className='mb-4 text-2xl font-light'>
-        A React app to search GitHub profiles and see profile details. This
-        project is part of the
+        A React app to search GitHub profiles 
         <a href='/'>
           {' '}
           React 
